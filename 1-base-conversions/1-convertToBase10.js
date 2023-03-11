485adadbd831a1b1d382cb77165cc237d7200dd6
sujal_shrestha <sujal.scxi121@amv.edu.np>
2023-03-11 07:45:34 +0545

    added convertToBase2

--- 1-base-conversions/1-convertToBase10.js
+++ 1-base-conversions/1-convertToBase10.js
@@ -2,62 +2,59 @@
 
 /******************************************************************************/
 
-const convertToBase10 = str => {
+const convertToBase10 = (str) => {
   // Your code here
   //convert hexadecimal to decimal
-  
-  if (str[0] === '0' && str[1] === 'x') {
-    let obj={
-      'a':10,
-      'b':11,
-      'c':12,
-      'd':13,
-      'e':14,
-      'f':15
-    }
+  if (str[0] === "0" && str[1] === "x") {
+    let obj = {
+      a: 10,
+      b: 11,
+      c: 12,
+      d: 13,
+      e: 14,
+      f: 15,
+    };
 
-    let decimal=0;
+    let decimal = 0;
     let hex = str.slice(2);
     for (let i = 0; i < hex.length; i++) {
-      if (obj[hex[i]]){
-       decimal+=Math.pow(16,hex.length-i-1)*obj[hex[i]];
+      if (obj[hex[i]]) {
+        decimal += Math.pow(16, hex.length - i - 1) * obj[hex[i]];
       } else {
-        decimal+=Math.pow(16,hex.length-i-1)*hex[i];
+        decimal += Math.pow(16, hex.length - i - 1) * hex[i];
       }
     }
     return decimal;
-   
   }
   //convert binary to decimal
-  if (str[0] === '0' && str[1] === 'b') {
+  if (str[0] === "0" && str[1] === "b") {
     let binary = str.slice(2);
     let decimal = 0;
     for (let i = 0; i < binary.length; i++) {
-      decimal+=Math.pow(2,binary.length-i-1)*binary[i];
+      decimal += Math.pow(2, binary.length - i - 1) * binary[i];
     }
     return decimal;
   }
-  
 };
 
 /******************************************************************************/
 
-console.log(convertToBase10('0b1100')); // 12
-console.log(convertToBase10('0b0101')); // 5
-console.log(convertToBase10('0b1000')); // 8
-console.log(convertToBase10('0b0111')); // 7
+console.log(convertToBase10("0b1100")); // 12
+console.log(convertToBase10("0b0101")); // 5
+console.log(convertToBase10("0b1000")); // 8
+console.log(convertToBase10("0b0111")); // 7
 
-console.log('––––––');
+console.log("––––––");
 
-console.log(convertToBase10('0b10100101')); // 165
-console.log(convertToBase10('0b11111111')); // 255
-console.log(convertToBase10('0b01010101')); // 85
-console.log(convertToBase10('0b00110011')); // 51
+console.log(convertToBase10("0b10100101")); // 165
+console.log(convertToBase10("0b11111111")); // 255
+console.log(convertToBase10("0b01010101")); // 85
+console.log(convertToBase10("0b00110011")); // 51
 
-console.log('––––––');
+console.log("––––––");
 
-console.log(convertToBase10('0xf')); // 15
-console.log(convertToBase10('0xfa')); // 250
-console.log(convertToBase10('0x1234')); // 4660
-console.log(convertToBase10('0xc9a1')); // 51617
-console.log(convertToBase10('0xbf12')); // 48914
+console.log(convertToBase10("0xf")); // 15
+console.log(convertToBase10("0xfa")); // 250
+console.log(convertToBase10("0x1234")); // 4660
+console.log(convertToBase10("0xc9a1")); // 51617
+console.log(convertToBase10("0xbf12")); // 48914
--- 1-base-conversions/2-convertToBase2.js
+++ 1-base-conversions/2-convertToBase2.js
@@ -1,9 +1,57 @@
 // Convert the integers in the console.logs below to base 2
 
+const convertToBase10 = (str) => {
+  // Your code here
+  //convert hexadecimal to decimal
+  if (str[0] === "0" && str[1] === "x") {
+    let obj = {
+      a: 10,
+      b: 11,
+      c: 12,
+      d: 13,
+      e: 14,
+      f: 15,
+    };
+
+    let decimal = 0;
+    let hex = str.slice(2);
+    for (let i = 0; i < hex.length; i++) {
+      if (obj[hex[i]]) {
+        decimal += Math.pow(16, hex.length - i - 1) * obj[hex[i]];
+      } else {
+        decimal += Math.pow(16, hex.length - i - 1) * hex[i];
+      }
+    }
+    return decimal;
+  }
+  //convert binary to decimal
+  if (str[0] === "0" && str[1] === "b") {
+    let binary = str.slice(2);
+    let decimal = 0;
+    for (let i = 0; i < binary.length; i++) {
+      decimal += Math.pow(2, binary.length - i - 1) * binary[i];
+    }
+    return decimal;
+  }
+};
 /******************************************************************************/
 
 const convertToBase2 = element => {
   // Your code here
+  if(typeof element === 'string'){
+    let decimal =convertToBase10(element);
+    return convertToBase2(decimal);
+  }
+  if (typeof element === 'number') {
+    let binary=[];
+    while (element > 0) {
+      binary.unshift(element % 2);
+      element = Math.floor(element / 2);
+    }
+    return `0b${binary.join('')}`;
+  }
+  
+
 };
 
 /******************************************************************************/
